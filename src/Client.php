29bab0072f601a72e56d206a7d82d8f2343300db
Ignacio Tolstoy <arkantos798@gmail.com>
2014-02-08 22:58:48 +0000

    Make classes psr2 compliant

--- src/Client.php
+++ src/Client.php
@@ -5,7 +5,7 @@ class Client
 {
     private $api_handler;
 
-    public static function create( $app_id, $secret_key, Adapter\Base $executor )
+    public static function create($app_id, $secret_key, Adapter\Base $executor)
     {
         $api_handler = new Request(
             $executor,
@@ -17,37 +17,37 @@ class Client
         return new self( $api_handler );
     }
 
-    public function __construct( Request $api_handler )
+    public function __construct(Request $api_handler)
     {
         $this->api_handler = $api_handler;
     }
 
-    public function getSerie( $id )
+    public function getSerie($id)
     {
-        return $this->getMedia( $id, Media\Types::SERIE );
+        return $this->getMedia($id, Media\Types::SERIE);
     }
 
-    public function getMovie( $id )
+    public function getMovie($id)
     {
-        return $this->getMedia( $id, Media\Types::MOVIE );
+        return $this->getMedia($id, Media\Types::MOVIE);
     }
 
-    public function getTvShow( $id )
+    public function getTvShow($id)
     {
-        return $this->getMedia( $id, Media\Types::TVSHOW );
+        return $this->getMedia($id, Media\Types::TVSHOW);
     }
 
-    public function getDocumentary( $id )
+    public function getDocumentary($id)
     {
-        return $this->getMedia( $id, Media\Types::DOCUMENTARY );
+        return $this->getMedia($id, Media\Types::DOCUMENTARY);
     }
 
-    private function getMedia( $id, $media_type )
+    private function getMedia($id, $media_type)
     {
         $factory = new Media\Factory();
 
         return $factory->create(
-            $this->api_handler->getBasicMedia( $id, $media_type )
+            $this->api_handler->getBasicMedia($id, $media_type)
         );
     }
 }
--- src/Media/Factory.php
+++ src/Media/Factory.php
@@ -3,20 +3,19 @@ namespace Naxhh\Seriesly\Media;
 
 class Factory
 {
-    public function create( $media_info )
+    public function create($media_info)
     {
-        // TODO: This should be refactored.
-        switch ( $media_info['mediaType'] ) {
+        switch ($media_info['mediaType']) {
             case Types::SERIE:
-                return new Serie( $media_info );
+                return new Serie($media_info);
             case Types::MOVIE:
-                return new Movie( $media_info );
+                return new Movie($media_info);
             case Types::TVSHOW:
-                return new TvShow( $media_info );
+                return new TvShow($media_info);
             case Types::DOCUMENTARY:
-                return new Documentary( $media_info );
+                return new Documentary($media_info);
             default:
-                throw new \UnexpectedValueException( 'Not valid media with type ' . $media_info['mediaType'] );
+                throw new \UnexpectedValueException('Not valid media with type ' . $media_info['mediaType']);
         }
     }
 }
